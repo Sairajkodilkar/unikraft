0d6e96d9106909792dc2f9cd180b8aa97f1ee9ed
Sairaj Kodilkar <skodilkar7@gmail.com>
2022-06-25 21:08:15 -0400

    lib/uklock: Complete the implementation of lock and unlock for reader-writer
    The Commit removes the bugs regarding flag updation and cleans the code
    written for reader writer lock and unlock function.
    
    Signed-off-by: Sairaj Kodilkar <skodilkar7@gmail.com>

--- lib/uklock/include/uk/rwlock.h
+++ lib/uklock/include/uk/rwlock.h
@@ -9,25 +9,30 @@
 /* TODO: in future give spin as well as wait options to the user 
  */
 
-#define READERS_SHIFT				(5)
-#define FLAG_MASK					(~(1 << READERS_SHIFT))
+#define FLAG_BITS		(5)
+#define READERS_SHIFT		(FLAG_BITS)
+#define FLAG_MASK		(~(1 << READERS_SHIFT))
 
-#define UK_RWLOCK_READ				(0x01)
+#define UK_RWLOCK_READ		(0x01)
 #define UK_RWLOCK_READ_WAITERS		(0x02)
 #define UK_RWLOCK_WRITE_WAITERS		(0x04)
-#define UK_RWLOCK_WRITE_SPINNERS	(0x08)
-#define UK_RWLOCK_WRITE_RECURSED	(0x10)
-#define UK_RWLOCK_WAITERS			(UK_RWLOCK_READ_WAITERS | UK_RWLOCK_WRITE_WAITERS)
+#define UK_RWLOCK_WAITERS		(UK_RWLOCK_READ_WAITERS | UK_RWLOCK_WRITE_WAITERS)
 
 #define UK_RW_READERS_LOCK(x)		((x) << READERS_SHIFT | UK_RWLOCK_READ)
-#define UK_RW_ONE_READER			(1 << READERS_SHIFT)
-#define UK_RW_READERS(x)			((x) >> READERS_SHIFT)
+#define UK_RW_ONE_READER		(1 << READERS_SHIFT)
+#define UK_RW_READERS(x)		((x) >> READERS_SHIFT)
 
-#define UK_RW_UNLOCK				(UK_RW_READERS_LOCK(0))
-#define OWNER(x)					(x & ~FLAG_MASK)
+#define UK_RW_UNLOCK		(UK_RW_READERS_LOCK(0))
+#define OWNER(x)		(x & ~FLAG_MASK)
 
-struct uk_rwlock {
-	volatile uintptr_t rwlock __aligned(8);
+#define _rw_can_read(rwlock) \
+	rwlock & (UK_RWLOCK_READ | UK_RWLOCK_WRITE_WAITERS) == UK_RWLOCK_READ
+
+#define _rw_can_write(v) \
+	((v & ~(UK_RWLOCK_WAITERS)) == UK_RW_UNLOCK);
+
+struct uk_rwlock __aligned(8) {
+	volatile uintptr_t rwlock;
 	unsigned int write_recurse;
 	struct uk_waitq shared;
 	struct uk_waitq exclusive;
@@ -35,16 +40,17 @@ struct uk_rwlock {
 
 void uk_rwlock_init(struct uk_rwlock *rwl);
 
-static inline 
-bool _rw_can_read(unsigned int rwlock)
-{
-	/* Give priority to the writers */
-	if(rwlock & (UK_RWLOCK_READ | UK_RWLOCK_WRITE_WAITERS 
-				| UK_RWLOCK_WRITE_SPINNERS) == UK_RWLOCK_READ) {
-		/* Success when there are no write pending */
-		return true;
+static inline
+void _rwlock_set_flag(uintptr_t *rwlock, uintptr_t flag) {
+
+	for(;;) {
+		v = *rwlock;
+		setv = v | flag;
+		if((v & flag)
+				|| ukarch_compare_exchange_sync(rwlock, v, setv) == setv)
+			break;
 	}
-	return false
+	return;
 }
 
 static inline 
@@ -63,22 +69,17 @@ void uk_rwlock_rlock(struct uk_rwlock *rwl)
 			}
 		}
 
-		/* Either the writer has the lock or the writer is waiting */
+		/* Set the read waiter flag if previously it is unset */
+		_rwlock_set_flag(&rwl->rwlock, UK_RWLOCK_READ_WAITERS);
 
-		if(!(v & UK_RWLOCK_READ_WAITERS)) {
-			setv = v | UK_RWLOCK_READ_WAITERS;
-			while(ukarch_compare_exchange_sync(&rwl->rwlock, v, setv) != setv) {
-				v = rwl->rwlock;
-				setv = v | UK_RWLOCK_READ_WAITERS;
-			}
-		}
-
+		/* Wait for the unlock event */
 		uk_waitq_wait_event(&rwl->shared, _rw_can_read(rwl->rwlock));
+
 		v = rwl->rwlock;
 	}
 	return;
 }
-
+   
 static inline
 void uk_rwlock_wlock(struct uk_rwlock *rwl)
 {
@@ -97,13 +98,11 @@ void uk_rwlock_wlock(struct uk_rwlock *rwl)
 
 	for(;;) {
 
-		mask = v & (UK_RWLOCK_WAITERS | UK_RWLOCK_WRITE_SPINNERS | UK_RWLOCK_WRITE_RECURSED);
-
 		/* If there is no reader or no owner and read flag is set then we can
 		 * acquire that lock
 		 */
-		if(v & ~mask == UK_RW_UNLOCK) {
-			setv = mask | stackbottom;
+		if(_rw_can_write(v)) {
+			setv = stackbottom | (v & UK_RWLOCK_WAITERS);
 			/* Try to set the owner field and flag mask except reader flag*/
 			if(ukarch_compare_exchange_sync(&rwl->lock, v, setv) == setv) {
 				ukarch_inc(&(rwl->write_recurse));
@@ -111,15 +110,9 @@ void uk_rwlock_wlock(struct uk_rwlock *rwl)
 			}
 		}
 
-		if(!(v & UK_RWLOCK_WRITE_WAITERS)) {
-			setv = v | UK_RWLOCK_WRITE_WAITERS;
-			while(ukarch_compare_exchange_sync(&rwl->lock, v, setv) != setv) {
-				v = rwl->rwlock;
-				setv = v | UK_RWLOCK_WRITE_WAITERS;
-			}
-		}
+		_rwlock_set_flag(&rwl->rwlock, UK_RWLOCK_WRITE_WAITERS);
 
-		uk_waitq_wait_event(&rwl->exclusive, ((rwl->rwlock & ~mask) == UK_RW_UNLOCK));
+		uk_waitq_wait_event(&rwl->exclusive, _rw_can_write(rwl->rwlock));
 		v = rwl->rwlock;
 	}
 }
@@ -140,6 +133,7 @@ void uk_rwlock_runlock(struct uk_rwlock *rwl)
 		return;
 
 	for(;;) {
+
 		setv = (v - UK_RW_ONE_READER) & ~(UK_RWLOCK_WAITERS);
 
 		if(UK_RW_READERS(setv) == 0 && v & UK_RWLOCK_WAITERS) {
@@ -157,7 +151,6 @@ void uk_rwlock_runlock(struct uk_rwlock *rwl)
 	}
 
 	uk_waitq_wake_up(queue);
-
 	return;
 }
 
@@ -177,14 +170,14 @@ void uk_rwlock_wunlock(struct uk_rwlock *rwl)
 
 	/* All recusive locks have been released, time to unlock*/
 	for(;;) {
-		setv = UK_RW_UNLOCK | 
-			(v & (UK_RWLOCK_WRITE_SPINNERS | UK_RWLOCK_WRITE_RECURSED));
+		setv = UK_RW_UNLOCK; 
 
-		if(v & UK_RWLOCK_WRITE_WAITERS) {
-			setv |= (v & UK_RWLOCK_READ_WAITERS);
-			queue = &rwl->exclusive;
-		} else if(v & UK_RWLOCK_READ_WAITERS){
+		if(v & UK_RWLOCK_WAITERS) {
 			queue = &rwl->shared;
+			if(v & UK_RWLOCK_WRITE_WAITERS) {
+				setv |= (v & UK_RWLOCK_READ_WAITERS);
+				queue = &rwl->exclusive;
+			}
 		}
 
 		if(ukarch_compare_exchange_sync(&rwl->rwlock, v, setv) == setv)
